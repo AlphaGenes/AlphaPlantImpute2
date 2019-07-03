d0304bb9823f26b020414211d02f28ed5e59e184
Steve Thorn <steve.thorn@ed.ac.uk>
2019-07-03 15:46:19 +0100

    Add code for simple imputation with haploidHMM

---
+++ pythonhmm-script.py
@@ -0,0 +1,10 @@
+"""Wrapper for pythonhmm"""
+
+from src import pythonhmm
+
+def main():
+    pythonhmm.main()
+
+if __name__ == '__main__':
+    main()
+    
---
+++ src/pythonhmm.py
@@ -0,0 +1,75 @@
+"""Simple haploid HMM implementation"""
+
+import argparse
+import numpy as np
+from .tinyhouse import Pedigree
+from .tinyhouse import InputOutput
+from .tinyhouse import BasicHMM
+
+
+# Create dummy profile decorator if not defined
+try:
+    profile
+except NameError as error:
+    def profile(dummy):
+        """Dummy decorator"""
+        return dummy
+
+
+def get_args():
+    """Get and parse command-line arguments"""
+    parser = argparse.ArgumentParser(description='')
+    core_parser = parser.add_argument_group("Core arguments")
+    core_parser.add_argument('-out', required=True, type=str, help='The output file prefix.')
+    InputOutput.addInputFileParser(parser)
+
+    pythonhmm_parser = parser.add_argument_group('pythonhmm Arguments')
+    pythonhmm_parser.add_argument('-hdthreshold', default=0.9, required=False, type=float,
+                                  help='Threshold for determining which haplotypes to include in the haplotype library')
+
+    return InputOutput.parseArgs("pythonhmm", parser)
+
+
+def get_haplotype_library(pedigree, threshold):
+    """Get haplotype library from high density individuals in a pedigree"""
+
+    haplotype_library = []
+    for individual in pedigree:
+        for haplotype in individual.haplotypes:
+            is_high_density = np.mean(haplotype != 9) >= threshold
+            if is_high_density:
+                haplotype_library += [haplotype]
+
+    return haplotype_library
+
+
+def do_imputation(haplotype_library, pedigree):
+    """Impute individuals using haploidHMM and a haplotype library"""
+
+    n_loci = pedigree.nLoci
+    for individual in pedigree:
+        dosages = np.full(n_loci, 0., dtype=np.float32)
+        for haplotype in individual.haplotypes:
+            dosages += BasicHMM.haploidHMM(haplotype, haplotype_library,
+                                           error=0.01, recombinationRate=1/n_loci,
+                                           callingMethod='dosages')
+        individual.dosages = dosages
+
+
+@profile
+def main():
+    """Main execution code"""
+
+    args = get_args()
+
+    pedigree = Pedigree.Pedigree()
+    InputOutput.readInPedigreeFromInputs(pedigree, args, genotypes=True, haps=True, reads=False)
+
+    haplotype_library = get_haplotype_library(pedigree, args.hdthreshold)
+    do_imputation(haplotype_library, pedigree)
+
+    pedigree.writeDosages(args.out + '.dosages')
+
+if __name__ == "__main__":
+    main()
+    
