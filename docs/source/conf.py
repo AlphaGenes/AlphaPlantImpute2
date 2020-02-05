ac73787ade8d93086dce4e946fd39a7d5fb22515
Steve.Thorn@ed.ac.uk <steve.thorn@ed.ac.uk>
2020-02-05 13:34:09 +0000

    Update doc version to match s/w

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Steve Thorn, Andrew Whalen, John M Hickey'
 author = 'Steve Thorn, Andrew Whalen, John M Hickey'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
