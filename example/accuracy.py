670a443d9381a87c6d7bc72ba772a5a34941d463
Steve Thorn <steve.thorn@ed.ac.uk>
2020-04-17 12:03:39 +0100

    Change accuracy.py to use genotypes and display various percentiles

--- example/accuracy.py
+++ example/accuracy.py
@@ -5,15 +5,20 @@ import numpy as np
 masked_genotypes = pd.read_csv('masked.genotypes', sep=' ', header=None, index_col=0)
 true_genotypes = pd.read_csv('true.genotypes', sep=' ', header=None, index_col=0)
 imputed_dosages = pd.read_csv('imputed.dosages', sep=' ', header=None, index_col=0)
+imputed_genotypes = pd.read_csv('imputed.genotypes', sep=' ', header=None, index_col=0)
 
 # Determine which individuals are genotyped at low- and high-density
 low_density = np.mean(masked_genotypes != 9, axis=1) < 0.9
 high_density = ~low_density
 
 # Accuracy is correlation between true genotypes and imputed dosages
-accuracy = true_genotypes.corrwith(imputed_dosages, axis=1)
+accuracy = true_genotypes.corrwith(imputed_genotypes, axis=1)
+
+
 
 # Report average accuracy for low- and high-density individuals
 print('Mean imputation accuracy')
 print(f'  high-density individuals {accuracy[high_density].mean():.4}')
 print(f'  low-density individuals  {accuracy[low_density].mean():.4}')
+
+print(accuracy[low_density].min(), np.quantile(accuracy[low_density], q=[0.05,0.5,0.95]))
