20d899cb89e76647619a3c9daa09d82a68b16d8f
Steve Thorn <steve.thorn@ed.ac.uk>
2020-09-09 10:23:41 +0100

    Update example script with correct accuracy

--- example/library/example.sh
+++ example/library/example.sh
@@ -12,7 +12,7 @@ AlphaPlantImpute2 -createlib -library lib1.ped -out lib2 -ped hd_2.ped -n_haplot
 # Impute using this library
 AlphaPlantImpute2 -impute -out imputed -library lib2.ped -ped masked.ped -n_haplotypes 20 -seed 42
 
-# Check accuracy - should be 0.9993 for HD and 0.9648 for LD (with seed 42)
+# Check accuracy - should be 1.0 for HD and 0.9638 for LD (with seed 42)
 AlphaPlantImpute2 -decode true.ped imputed.ped
 python ../accuracy.py
 
